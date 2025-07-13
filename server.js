2e07934bcf58074d33d257fc459fd893805ff7c6
Jazyell Marcus <jazyellmarcus94@gmail.com>
2025-07-13 03:05:41 -0300

    Conectando ao banco da Railway

--- server.js
+++ server.js
@@ -16,11 +16,13 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 // Conexão com o banco de dados
+
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Jazy2924',
-    database: 'delivery',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
